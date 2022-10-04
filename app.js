5ce5784e1f786318b26312f595b4baceef339ee2
Cesar Carreras <cesar.carreras95@gmail.com>
2022-10-03 19:02:08 -0500

    changed data source from js file to raw html

--- app.js
+++ app.js
@@ -3,27 +3,3 @@ var loader = document.getElementById("loader-container");
 window.addEventListener("load", function () {
   loader.style.display = "none";
 });
-
-async function appendData() {
-  var mainContainer = document.getElementById("myData");
-  for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = `
-                       <div class="col d-flex flex-column p-3 align-items-center">
-                          <a href="${data[i].link}">
-                            <img src="${data[i].image}" class="img-thumbnail border-0 hover-img" alt="${data[i].name} image" />
-                          </a>
-                          <p class="mt-2">${data[i].name}</p>
-                      </div>
-                              `;
-
-    div.classList.add(
-      "animate__animated",
-      "animate__zoomIn",
-      "animate__delay-0.5s"
-    );
-    mainContainer.appendChild(div);
-  }
-}
-
-appendData();
